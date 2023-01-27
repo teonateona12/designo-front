229e5dceb201a4e55ca666a18fab702ab07fed6e
Teona Piranishvili <teona.piranishvili1@gmail.com>
2023-01-27 13:47:14 +0400

    style: style three container

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -97,6 +97,65 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <div className="p-6 md:px-9 py-12 xl:py-[60px] xl:px-[170px] xl:flex xl:gap-10">
+        <div className="flex flex-col gap-6 md:flex-row md:gap-10 xl:flex-col">
+          <div>
+            <img
+              className="m-auto"
+              src="http://localhost:5000/designo/image1.png"
+            />
+          </div>
+          <div>
+            <h1 className="text-center tracking-[5px] md:text-left xl:text-center">
+              PASSIONATE
+            </h1>
+            <p className="text-center mt-4 md:text-left  xl:text-center">
+              Each project starts with an in-depth brand research to ensure we
+              only create products that serve a purpose. We merge art, design,
+              and technology into exciting new solutions.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-6 my-20 md:flex-row md:gap-10 xl:my-0 xl:flex-col">
+          <div>
+            <img
+              className="m-auto"
+              src="http://localhost:5000/designo/image2.png"
+            />
+          </div>
+          <div>
+            <h1 className="text-center tracking-[5px] md:text-left xl:text-center">
+              RESOURCEFUL
+            </h1>
+            <p className="text-center mt-4 md:text-left  xl:text-center">
+              Everything that we do has a strategic purpose. We use an agile
+              approach in all of our projects and value customer collaboration.
+              It guarantees superior results that fulfill our clients’ needs.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-6 md:flex-row md:gap-10 xl:flex-col">
+          <div>
+            <img
+              className="m-auto"
+              src="http://localhost:5000/designo/image3.png"
+            />
+          </div>
+          <div>
+            <h1 className="text-center tracking-[5px] md:text-left xl:text-center">
+              FRIENDLY
+            </h1>
+            <p className="text-center mt-4 md:text-left xl:text-center">
+              We are a group of enthusiastic folks who know how to put people
+              first. Our success depends on our customers, and we strive to give
+              them the best experience a company can provide.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
